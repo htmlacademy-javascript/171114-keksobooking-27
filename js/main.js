ab07b06376a7528f46f8b427d2bed8b3a3512ffc
redcat <redcat-3@yandex.ru>
2022-10-02 23:27:20 +0800

    добавляет ещё одну функцию

--- js/main.js
+++ js/main.js
@@ -2,14 +2,35 @@ function getRandomNumber(min, max) {
   if (min < 0 || max < 0) {
     return NaN;
   }
-  const result = Math.random() * (max - min);
-  if (result === 0) {
+  if ((max - min) === 0) {
     return NaN;
   }
-  if (result > 0) {
-    return result + min;
+  const result = Math.random() * (max - min);
+  if ((max - min) > 0) {
+    return Math.round(result + min);
   }
-  return max - result;
+  return Math.round(max - result);
 }
 
+function getRandomFloat(min, max, numberOfDecimalPlases) {
+  if (min < 0 || max < 0 || numberOfDecimalPlases < 0) {
+    return NaN;
+  }
+
+  if ((max - min) === 0) {
+    return NaN;
+  }
+  let count = 1;
+  for (let i = 0; i < numberOfDecimalPlases; i++) {
+    count = count * 10;
+  }
+  const result = Math.random() * (max - min);
+
+  if ((max - min) > 0) {
+
+    return Math.round((result + min) * count) / count;
+  }
+  return Math.round((max - result) * count) / count;
+}
 getRandomNumber(1, 5);
+getRandomFloat(1, 5, 1);
