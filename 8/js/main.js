3117fe66ebbc1f04000d80c1c0790c8d6294d192
Keks <keks@htmlacademy.ru>
2022-10-19 14:14:27 +0300

    :heavy_check_mark: Сборка #8

---
+++ 8/js/main.js
@@ -0,0 +1,12 @@
+import {getAdFormDisabled} from './ad-form.js';
+import {getAdFormActive} from './ad-form.js';
+import {createAdvertisements} from './data.js';
+import {showAdvertisment} from './card.js';
+
+const mapCanvas = document.querySelector('#map-canvas');
+const similarAdvertisements = createAdvertisements();
+mapCanvas.appendChild(showAdvertisment(similarAdvertisements[0]));
+
+getAdFormDisabled();
+getAdFormActive();
+
