c0dab9a7f95ad2339a73f6ec2b7fa9b1373021d9
redcat <redcat-3@yandex.ru>
2022-10-19 16:47:33 +0800

    правки по 7 заданию

--- js/card.js
+++ js/card.js
@@ -1,10 +1,9 @@
-
+import {houseTypes} from './data.js';
 
 const showAdvertisment = (advertisement) => {
   const templateFragment = document.querySelector('#card').content;
   const fragmentList = document.createDocumentFragment();
   const advertisementElement = templateFragment.cloneNode(true);
-  const type = advertisement.offer.type;
   const features = advertisement.offer.features;
   const featureList = advertisementElement.querySelectorAll('.popup__feature');
   const photos = advertisementElement.querySelector('.popup__photos');
@@ -12,22 +11,7 @@ const showAdvertisment = (advertisement) => {
   advertisementElement.querySelector('.popup__title').textContent = advertisement.offer.title;
   advertisementElement.querySelector('.popup__text--address').textContent = advertisement.offer.address;
   advertisementElement.querySelector('.popup__text--price').textContent = `${advertisement.offer.price} ₽/ночь`;
-  switch (type) {
-    case 'flat':
-      advertisementElement.querySelector('.popup__type').textContent = 'Квартира';
-      break;
-    case 'bungalow':
-      advertisementElement.querySelector('.popup__type').textContent = 'Бунгало';
-      break;
-    case 'house':
-      advertisementElement.querySelector('.popup__type').textContent = 'Дом';
-      break;
-    case 'palace':
-      advertisementElement.querySelector('.popup__type').textContent = 'Дворец';
-      break;
-    case 'hotel':
-      advertisementElement.querySelector('.popup__type').textContent = 'Отель';
-  }
+  advertisementElement.querySelector('.popup__type').textContent = houseTypes[advertisement.offer.type];
   advertisementElement.querySelector('.popup__text--capacity').textContent = `${advertisement.offer.rooms} комнаты для ${advertisement.offer.guests} гостей`;
   advertisementElement.querySelector('.popup__text--time').textContent = `Заезд после ${advertisement.offer.checkin}, выезд до ${advertisement.offer.checkout}`;
   featureList.forEach((featureListItem) => {
@@ -40,9 +24,10 @@ const showAdvertisment = (advertisement) => {
     }
   });
   advertisementElement.querySelector('.popup__description').textContent = advertisement.offer.description;
+  photos.innerHTML = '';
   advertisement.offer.photos.forEach((img) => {
     const photosElement = photo.cloneNode(true);
-    photosElement.setAttribute('src', img);
+    photosElement.src = img;
     photos.appendChild(photosElement);
   });
   advertisementElement.querySelector('.popup__avatar').setAttribute('src',advertisement.author.avatar);
