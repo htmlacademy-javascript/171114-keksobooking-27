60502e786cc40dec393ee48ac4f3ea1248f4a35a
redcat <redcat-3@yandex.ru>
2022-10-26 20:58:52 +0800

    добавляет валидацию минимальной цены и времени въезда/выезда

--- js/form.js
+++ js/form.js
@@ -1,7 +1,17 @@
 const adForm = document.querySelector('.ad-form');
+const timein = adForm.querySelector('#timein');
+const timeout = adForm.querySelector('#timeout');
 
 const MAX_ROOM_NUMBER = '100';
 const NOT_FOR_GUEST = '0';
+
+const priceOfTypes = {
+  flat: 1000,
+  bungalow: 0,
+  house: 5000,
+  palace: 10000,
+  hotel: 3000,
+};
 
 const pristine = new Pristine(adForm, {
   classTo: 'ad-form__element',
@@ -13,6 +23,8 @@ const pristine = new Pristine(adForm, {
 });
 
 const roomNumber = adForm.querySelector('#room_number');
+const type = adForm.querySelector('#type');
+const price = adForm.querySelector('#price');
 
 const validateCapacity = (value) => {
   if (roomNumber.value === MAX_ROOM_NUMBER) {
@@ -24,13 +36,36 @@ const validateCapacity = (value) => {
   return value <= roomNumber.value;
 };
 
+const validatePrice = () => {
+  price.min = `${priceOfTypes[type.value]}`;
+  if (priceOfTypes[type.value] <= price.value)
+    return true;
+  return false;
+};
+
 pristine.addValidator(
   adForm.querySelector('#capacity'),
   validateCapacity,
   'Количество мест меньше или равно количеству комнат. 100 комнат — не для гостей (0 гостей)'
 );
 
+pristine.addValidator(
+  price,
+  validatePrice,
+  `Минимальная цена - ${priceOfTypes[type.value]}`
+);
+
 adForm.addEventListener('submit', (evt) => {
   evt.preventDefault();
   pristine.validate();
+});
+
+timein.addEventListener('change', (evt) => {
+  evt.preventDefault();
+  timeout.value = timein.value;
+});
+
+timeout.addEventListener('change', (evt) => {
+  evt.preventDefault();
+  timein.value = timeout.value;
 });
